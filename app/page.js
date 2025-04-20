243d27223693b3b4dfeecf4ff382e4a1d7273ed6
Priyanshu Sabaar <priyanshu.tca2263106@tmu.ac.in>
2025-04-20 18:39:14 +0530

    layout.js page.js upadted

--- app/page.js
+++ app/page.js
@@ -4,14 +4,14 @@ import Header from "./Components/Header";
 const Home = () => {
   return (
     <div>
-      <Header/>
+      
       <main className="flex-1 bg-gray-100">
         <div className="max-w-4xl mx-auto p-8">
-          <h2 className="text-xl font-bold mb-4 text-black">Main Content</h2>
+          <h2 className="text-xl font-bold mb-4 text-black sapce-x-6 ">Main Content</h2>
           <p className="text-black">This section takes up the remaining height of the screen.</p>
         </div>
       </main>
-      <Footer/>
+      
     </div>
   );
 };
